531cf0b5e0a7516cb075fe0825a4e3d7f19eebba
Renny Gorozabel <rennygorozabel@hotmail.com>
2019-04-10 16:00:34 -0500

    Métodos y status

--- 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, HttpCode, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,30 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post()
+  @HttpCode(200)
+  postHello(){
+    return 'Hola mundo en post';
+  }
+}
+
+@nombreDecoradorClase
+class usuario{
+  @decoradorAtributo
+  atributoPublico; //sin modificador, por defecto es publico
+  private atributoPrivado;
+  protected atributoProtegido;
+
+  constructor(@Parametro() atributoPublico, @OtroParametro() atributoPrivado, atributoProtegido){
+    this.atributoPublico = atributoPublico;
+    this.atributoPrivado = atributoPrivado;
+    this.atributoProtegido = atributoProtegido;
+  }
+
+   @MetodoA() /* @decorador funcion*/
+  public metodoPublico(){}
+  @MetodoB()
+  private metodoPrivado(){}
+  protected metodoProtegido(){}
 }
