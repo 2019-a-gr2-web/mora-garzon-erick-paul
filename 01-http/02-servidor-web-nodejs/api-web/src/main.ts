35542d2d3b85445a24a132b452833d94557f388b
Erick Mora <erick_3069@hotmail.com>
2019-05-07 16:00:08 -0500

    etiquetas

--- 01-http/02-servidor-web-nodejs/api-web/src/main.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/main.ts
@@ -2,20 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {NestExpressApplication} from "@nestjs/platform-express";
 import {join} from "path";
+
+import * as express from 'express';
+import * as path from 'path';
+import * as favicon from 'serve-favicon';
+
 //import * as cookieParser from 'cookie-parser';
 const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule) as NestExpressApplication;
 
-  app.use(cookieParser('Secreto'));
+
 
   //@ts-ignore
   //app.set('view engine', 'ejs');
     //app.engine('view engine', 'ejs');
+    app.use(favicon(path.join(__dirname, '..', 'publico', 'imagenes', 'pokebola.ico')));
+
+
+    app.use(cookieParser('Secreto'));
+
+
     app.setViewEngine('ejs');
     app.setBaseViewsDir(join( __dirname, '..', 'views'));
 
+    app.use(express.static('publico'));
 
   await app.listen(3000);
 }
