3b4eb2bbc73d95ca747ebc3991507934ec31d2b9
ErickMora <erick_3069@hotmail.com>
2019-05-22 00:25:49 -0500

    deber página web con recursos

--- deberes/03-pagina-web/api-pag-web/src/main.ts
+++ deberes/03-pagina-web/api-pag-web/src/main.ts
@@ -3,11 +3,19 @@ import { AppModule } from './app.module';
 import {NestExpressApplication} from "@nestjs/platform-express";
 import {join} from "path";
 
+import * as express from 'express';
+import * as path from 'path';
+import * as favicon from 'serve-favicon';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule) as NestExpressApplication;
 
+  app.use(favicon(path.join(__dirname, '..', 'publico', 'imagenes', 'blogger.ico')));
+
   app.setViewEngine('ejs');
   app.setBaseViewsDir(join( __dirname, '..', 'views'));
+
+  app.use(express.static('publico'));
 
 
   await app.listen(3000);
